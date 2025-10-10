c02002d17065bb4a042c451ce96a391c7d380fed
juliencohen <Julien.Cohen@univ-nantes.fr>
2025-10-10 08:34:30 +0200

    Simplification.

--- samples/agents/helloworld/__main__.py
+++ samples/agents/helloworld/__main__.py
@@ -14,7 +14,7 @@ from agent_executor import (
 
 
 if __name__ == '__main__':
-    # --8<-- [start:AgentSkill]
+
     number_provider_skill = AgentSkill(
         id='number-provider',
         name='provide a number',
@@ -22,17 +22,9 @@ if __name__ == '__main__':
         tags=['get', 'state'],
         examples=['(achieve, ping)', '(tell, ready)', '(ask,secret)'],
     )
-    # --8<-- [end:AgentSkill]
 
-    extended_skill = AgentSkill(
-        id='super_hello_world',
-        name='Returns a SUPER Hello World',
-        description='A more enthusiastic greeting, only for authenticated users.',
-        tags=['hello world', 'super', 'extended'],
-        examples=['super hi', 'give me a super hello'],
-    )
 
-    # --8<-- [start:AgentCard]
+
     # This will be the public-facing agent card
     public_agent_card = AgentCard(
         name='State Agent',
@@ -42,26 +34,10 @@ if __name__ == '__main__':
         default_input_modes=['text'],
         default_output_modes=['text'],
         capabilities=AgentCapabilities(streaming=True, push_notifications=True),
-        skills=[number_provider_skill],  # Only the basic skill for the public card
+        skills=[number_provider_skill],
         supports_authenticated_extended_card=True,
     )
-    # --8<-- [end:AgentCard]
 
-    # This will be the authenticated extended agent card
-    # It includes the additional 'extended_skill'
-    specific_extended_agent_card = public_agent_card.model_copy(
-        update={
-            'name': 'State Ping Agent - Extended Edition',  # Different name for clarity
-            'description': 'The full-featured state ping agent for authenticated users.',
-            'version': '1.0.1',  # Could even be a different version
-            # Capabilities and other fields like url, default_input_modes, default_output_modes,
-            # supports_authenticated_extended_card are inherited from public_agent_card unless specified here.
-            'skills': [
-                number_provider_skill,
-                extended_skill,
-            ],  # Both skills for the extended card
-        }
-    )
 
     request_handler = DefaultRequestHandler(
         agent_executor=StateAgentExecutor(),
@@ -71,7 +47,6 @@ if __name__ == '__main__':
     server = A2AStarletteApplication(
         agent_card=public_agent_card,
         http_handler=request_handler,
-        extended_agent_card=specific_extended_agent_card,
     )
 
     uvicorn.run(server.build(), host='0.0.0.0', port=9999)
